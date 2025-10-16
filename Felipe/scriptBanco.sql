1a7aa9e9bf8be7d31946068f67586f36b79d1978
FIGFelip <felipegonc2020@gmail.com>
2025-10-16 20:52:27 -0300

    fix: Adicionada cláusula UNSIGNED nas FKs das tabelas correspondentes, exceto nas colunas com valores DECIMAL.

--- Felipe/scriptBanco.sql
+++ Felipe/scriptBanco.sql
@@ -12,8 +12,8 @@ CREATE TABLE `filmes`(
     `titulo` VARCHAR(255) NOT NULL,
     `genero` VARCHAR(255) NOT NULL,
     `tipo_filme` VARCHAR(255) NOT NULL,
-    `classificacao` SMALLINT NOT NULL,
-    `duracao` INT NOT NULL,
+    `classificacao` SMALLINT UNSIGNED NOT NULL,
+    `duracao` INT UNSIGNED NOT NULL,
     `sinopse` TEXT NOT NULL
 );
 CREATE TABLE `compra`(
@@ -23,7 +23,7 @@ CREATE TABLE `compra`(
     `status_compra` VARCHAR(20) NOT NULL,
     `data_canc` DATETIME NULL,
     `motivo_canc` TEXT NULL,
-    `id_usuario` INT NOT NULL
+    `id_usuario` INT UNSIGNED NOT NULL
 );
 CREATE TABLE `pagamento`(
     `id_pagamento` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -31,7 +31,7 @@ CREATE TABLE `pagamento`(
     `valor_pagamento` DECIMAL(10, 2) NOT NULL,
     `status_pagamento` VARCHAR(255) NOT NULL,
     `data_pagamento` DATETIME NOT NULL,
-    `id_compra` BIGINT NOT NULL
+    `id_compra` BIGINT UNSIGNED NOT NULL
 );
 ALTER TABLE
     `pagamento` ADD UNIQUE `pagamento_id_compra_unique`(`id_compra`);
@@ -40,16 +40,16 @@ CREATE TABLE `sessao`(
     `data_sessao` DATE NOT NULL,
     `hora_sessao` TIME NOT NULL,
     `tipo_sessao` VARCHAR(255) NOT NULL,
-    `sala` SMALLINT NOT NULL,
+    `sala` SMALLINT UNSIGNED NOT NULL,
     `preco` DECIMAL(10, 2) NOT NULL,
-    `id_filme` INT NOT NULL
+    `id_filme` INT UNSIGNED NOT NULL
 );
 CREATE TABLE `assento`(
     `id_assento` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `fileira` CHAR(1) NOT NULL,
-    `num_assento` TINYINT NOT NULL,
+    `num_assento` TINYINT UNSIGNED NOT NULL,
     `status_assento` VARCHAR(255) NOT NULL DEFAULT 'livre',
-    `id_sessao` INT NOT NULL
+    `id_sessao` INT UNSIGNED NOT NULL
 );
 ALTER TABLE
     `assento` ADD UNIQUE `assento_fileira_num_assento_id_sessao_unique`(
@@ -62,8 +62,8 @@ CREATE TABLE `ingresso`(
     `tipo` VARCHAR(255) NOT NULL,
     `status_ingresso` VARCHAR(255) NOT NULL DEFAULT 'disponivel',
     `valor_ingresso` DECIMAL(8, 2) NOT NULL,
-    `id_sessao` INT NOT NULL,
-    `id_compra` BIGINT NOT NULL
+    `id_sessao` INT UNSIGNED NOT NULL,
+    `id_compra` BIGINT UNSIGNED NOT NULL
 );
 ALTER TABLE
     `ingresso` ADD UNIQUE `ingresso_id_sessao_tipo_id_compra_unique`(`id_sessao`, `tipo`, `id_compra`);
